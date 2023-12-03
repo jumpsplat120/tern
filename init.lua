ffbc44534613514c3bfba95709251e92808e6149
jumpsplat120 <jumpsplat120@yahoo.com>
2023-12-02 17:53:40 -0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+---Returns the first value if bool is truthy, and returns the second if falsey. This is a useful shorthand
+---for certain cases, where you might want to return a falsey value, such as `b = a == 1 and false or a`,
+---as in the example, `b` would be `a`, not `false`, due to how `and/or` operators actually work.
+---@param bool any A value or expression to be evaulated as a boolean. Follows truthiness rules, and does not need to explicitly be `true` or `false`.
+---@param a any The value to return if `bool` is truthy.
+---@param b any The value to return if `bool` is falsey.
+---@return any
+---@nodiscard
+tern = function(bool, a, b)
+    if bool then return a end
+
+    return b
+end
